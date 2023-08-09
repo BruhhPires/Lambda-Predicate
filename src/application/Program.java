00b36d662a7ef824843fda930a671a53cdb81929
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-09 13:16:42 -0300

    Declared Lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -17,7 +18,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(Product::nonStaticProductPredicate); // AQUI JÁ USAMOS O METODO DE REFERENCIA CLASSE::METODO
+		Predicate<Product> pred = p -> p.getPrice() >= 100.0; // DECLARAÇÃO EXPRESSÃO LAMBDA 
+		
+		list.removeIf(pred); 
 
 		for(Product p: list) {
 			System.out.println(p);
